fb67fb06a61243670052d3988be3c1aed784619a
Nishita Rani <92663360+Nishita0807@users.noreply.github.com>
2023-10-01 17:14:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-//your JS code here. If required.
+function updateClock() {
+    const timerElement = document.getElementById('timer');
+    const now = new Date();
+
+    // Format the date and time as a string
+    const dateTimeString = now.toLocaleString();
+
+    // Update the timer element with the current date and time
+    timerElement.textContent = dateTimeString;
+}
+
+// Update the clock immediately and then every second
+updateClock();
+setInterval(updateClock, 1000);
